a61e6340bd7bbcb6ebcf90a5b28218dd0354e209
luisy9 <luisdeharo4@gmail.com>
2024-01-01 22:56:57 +0100

    subscribe Observable changeQuantity

--- PEC6_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ArticleEventData } from '../article/article.component';
 import { ArticleServiceService } from '../article-service.service';
@@ -18,28 +18,27 @@ export interface Article {
   styleUrls: ['./article-list.component.css']
 })
 
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
   public articlesData$!: Observable<Article[]>;
   public articleChange$!: Observable<QuantityArticle[]>;
   idArticle: number = 0;
-  articleId: [] = [];
-  actualId: { id: number; count: number }[] = [];
+  public articleId: [] = [];
+  actualId: [] = [];
 
   public articlesService = inject(ArticleServiceService);
 
   constructor() {
     this.articlesData$ = this.articlesService.getArticle();
+    this.articleChange$ = this.articlesService._quantityArticle;
   }
 
   public objectChild($event: ArticleEventData): void {
-    this.articlesService.changeQuantity($event)
-    .subscribe(
-      (data: QuantityArticle[]) => {
-        this.articleId.push(data);
-      },
-      (error) => {
-        console.error(error);
-      });
-    console.log(this.articleChange$);
+    this.articlesService.changeQuantity($event);
+  }
+
+  ngOnInit(): void {
+    this.articleChange$.subscribe((e) => {
+      console.log(e)
+    });
   }
 }
--- PEC6_Ej_Prac/ecommerce/src/app/article-service.service.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/article-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Article } from './article-list/article-list.component';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 export interface OperationQuantity {
   articuleId: number;
@@ -23,34 +23,47 @@ export class ArticleServiceService {
   { id: 2, articulo: 'Cascos gaming', imgArticle: "https://imgs.search.brave.com/R0TErqPQRL8eukHShkjOpjFaZZYSPdvRdWQd4kT7QZE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFsNE1yUCtYTUwu/anBn", price: 20, onSale: true },
   { id: 3, articulo: 'Pantalla gaming', imgArticle: "https://imgs.search.brave.com/atxwJUNu94DnuityOIMddoy609ssIvg9_RZpyz2lYpc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLmJs/b2dzLmVzLzZlZjk4/Zi84MTdpYndrYi05/bC5fYWNfc2wxNTAw/Xy80NTBfMTAwMC53/ZWJw", price: 50, onSale: false }];
 
-  public actualId: { id: number; quantity: number }[] = [];
+  public actualId: QuantityArticle[] = [];
+  private quantityArticles: BehaviorSubject<QuantityArticle[]>;
+
+  constructor() {
+    this.quantityArticles = new BehaviorSubject<QuantityArticle[]>([]);
+  }
 
   getArticle(): Observable<Article[]> {
     return of(this.articleList);
   }
 
-  changeQuantity({ articuleId, operation }: OperationQuantity): Observable<QuantityArticle[]> {
+  changeQuantity({ articuleId, operation }: OperationQuantity): void {
+    const existingItem = this.actualId.find(e => e.id === articuleId);
     if (operation === 'sum') {
-      const existingItem = this.actualId.find(e => e.id === articuleId);
-      if (!existingItem) {
-        this.actualId.push({ id: articuleId, quantity: 1 });
-      } else {
-        // existingItem.count += 1;
-      }
+      this.handleOperationSum(articuleId, existingItem);
     } else if (operation === 'res') {
-      const existingItem = this.actualId.find(e => e.id === articuleId);
-      if (!existingItem) {
-        this.actualId.push({ id: articuleId, quantity: 1 });
-      } else {
-        if (existingItem.quantity > 0) {
-          existingItem.quantity -= 1;
-        }
-      }
+      this.handleOperationRes(articuleId, existingItem);
     }
-    return of(this.actualId);
+    this.quantityArticles.next({ ...this.actualId });
   }
 
-  // createArticle() {
+  //Function of add
+  handleOperationSum(articuleId: number, itemSelected: any): any {
+    if (!itemSelected) {
+      this.actualId.push({ id: articuleId, quantity: 1 });
+    } else {
+      itemSelected.quantity += 1;
+    }
+  }
 
-  // }
+  //Function for decrement
+  handleOperationRes(articleId: number, itemSelected: any): any {
+    if (!itemSelected) {
+      this.actualId.push({ id: articleId, quantity: 0 });
+    } else {
+      itemSelected.quantity -= 1;
+    }
+  }
+
+
+  get _quantityArticle(): Observable<QuantityArticle[]> {
+    return this.quantityArticles.asObservable();
+  }
 }
